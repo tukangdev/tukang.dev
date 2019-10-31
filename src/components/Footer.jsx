4f4e1161a4f604c81c0be9b0eb3c340e44981b03
Amin Roslan <amnrsln@gmail.com>
2019-11-01 02:54:43 +0800

    Changes were made

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,63 +1,67 @@
-import React from "react";
-import { Link } from "gatsby";
-import styled from "@emotion/styled";
-import colors from "styles/colors";
-import Logo from "components/_ui/Logo";
-import spooch from "images/oscar-icon.png"
+import React from "react"
+import { Link } from "gatsby"
+import styled from "@emotion/styled"
+import colors from "styles/colors"
+import Logo from "components/_ui/Logo"
 
 const FooterContainer = styled("div")`
-    padding-top: 3.75em;
-    padding-bottom: 3em;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  padding-top: 3.75em;
+  padding-bottom: 3em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-    svg {
-        max-width: 50px;
-    }
+  svg {
+    max-width: 150px;
+  }
 `
 
 const FooterAuthor = styled("a")`
-    font-size: 0.75em;
-    color: ${colors.grey700};
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    text-decoration: none;
-    margin-top: 1.5em;
+  font-size: 0.75em;
+  color: ${colors.grey700};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-decoration: none;
+  margin-top: 1.5em;
 
-     &:hover {
-         color: ${colors.blue900};
+  &:hover {
+    color: ${colors.blue900};
 
-        .FooterSpooch {
-            animation-name: rotate;
-            animation-duration: 1.5s;
-            animation-iteration-count: infinite;
-            animation-timing-function: linear;
-        }
+    .FooterSpooch {
+      animation-name: rotate;
+      animation-duration: 1.5s;
+      animation-iteration-count: infinite;
+      animation-timing-function: linear;
     }
+  }
 
-    @keyframes rotate {
-        from {transform: rotate(0deg);}
-        to {transform: rotate(360deg);}
+  @keyframes rotate {
+    from {
+      transform: rotate(0deg);
     }
+    to {
+      transform: rotate(360deg);
+    }
+  }
 `
 
-const FooterSpooch = styled("img")`
-    max-width: 33px;
-    margin-top: 0.25em;
+const FooterSpooch = styled("div")`
+  font-size: 2rem;
+  max-width: 33px;
+  margin-top: 0.25em;
 `
 
 const Footer = () => (
-    <FooterContainer>
-        <Link to="/">
-            <Logo />
-        </Link>
-        <FooterAuthor href="https://marguerite.io">
-            © 2019 — Designed & developed by Marguerite Roth
-            <FooterSpooch className="FooterSpooch" src={spooch} />
-        </FooterAuthor>
-    </FooterContainer>
+  <FooterContainer>
+    <Link to="/">
+      <Logo />
+    </Link>
+    <FooterAuthor href="https://aminroslan.com">
+      © 2019 — Designed & developed by Amin Roslan
+      <FooterSpooch className="FooterSpooch">💩</FooterSpooch>
+    </FooterAuthor>
+  </FooterContainer>
 )
 
-export default Footer;
+export default Footer
